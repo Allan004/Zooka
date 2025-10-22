47bb192915efc504f1b4a1d727c9172def56ca95
Allan004 <allanbelchior04@gmail.com>
2025-10-21 21:34:33 -0300

    teste 2 com mixed

---
+++ ConsultaFornecedor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zooka
+{
+    public partial class ConsultaFornecedor: Form
+    {
+        consulta_cliente teste = new consulta_cliente();
+        public ConsultaFornecedor()
+        {
+            InitializeComponent();
+            dtgCliente.DataSource = teste.InsertGeral("fornecedor");
+            if (ttt = true)
+            {
+                button3.Visible = false;
+                button2.Visible = false;
+                button4.Visible = false;
+            }
+        }
+        bool ttt = false;
+        public void receberbool(bool tt)
+        {
+            ttt = tt;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtgCliente_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dtgCliente.CurrentRow != null)
+            {
+                string id = Convert.ToString(dtgCliente.SelectedRows[0].Cells["id_fornecedor"].Value);
+
+
+                var confirmResult = MessageBox.Show("Tem certeza que deseja excluir esta linha?", "Confirmação", MessageBoxButtons.YesNo);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    teste.ExcluirLinha("fornecedor", "id_fornecedor", id);
+                    dtgCliente.Refresh();
+
+                }
+                else
+                {
+                    MessageBox.Show("Selecione um produto para remover.");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
